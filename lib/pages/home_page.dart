f7fbad211abf73cededff9492838251dc7ffe6c1
Sonia Justiniano <sjustinianoayte@gmail.com>
2023-04-04 19:06:44 -0500

    Mods

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:codigo_books1/widgets/item_slider_widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,121 +12,180 @@ class HomePage extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     double pyth = sqrt(pow(height, 2) + pow(width, 2));
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: pyth * 0.42,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  "https://images.pexels.com/photos/1893264/pexels-photo-1893264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: pyth * 0.42,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                    "https://images.pexels.com/photos/1893264/pexels-photo-1893264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                  ),
+                ),
+              ),
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 12.0,
+                      ),
+                      decoration: BoxDecoration(boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.25),
+                          blurRadius: 15.0,
+                          offset: const Offset(4, 4),
+                        ),
+                      ]),
+                      width: pyth * 0.35,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: "Buscar libro...",
+                          hintStyle: const TextStyle(
+                            fontSize: 14.0,
+                          ),
+                          suffixIcon: const Icon(
+                            Icons.search,
+                            size: 19,
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(22.0),
+                      child: Row(
+                        children: const [
+                          Text(
+                            "Buscar \ntus libros \nfavoritos",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 36.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            child: SafeArea(
+            Padding(
+              padding: const EdgeInsets.all(14.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 12.0,
-                    ),
-                    decoration: BoxDecoration(boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.25),
-                        blurRadius: 15.0,
-                        offset: const Offset(4, 4),
-                      ),
-                    ]),
-                    width: pyth * 0.35,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: "Buscar libro...",
-                        hintStyle: const TextStyle(
-                          fontSize: 14.0,
-                        ),
-                        suffixIcon: const Icon(
-                          Icons.search,
-                          size: 19,
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    "Mis libros favoritos",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(22.0),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
                     child: Row(
-                      children: const [
-                        Text(
-                          "Buscar \ntus libros \nfavoritos",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 36.0,
-                            fontWeight: FontWeight.w900,
-                          ),
-                        ),
+                      children: [
+                        ItemSliderWidget(),
+                        ItemSliderWidget(),
+                        ItemSliderWidget(),
                       ],
                     ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "Lista General",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.network(
+                          "https://images.pexels.com/photos/5913138/pexels-photo-5913138.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                          width: pyth * 0.12,
+                          height: pyth * 0.16,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              " Ther Lord of the ring",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const Text(
+                              " Sonia Justiniano",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              " Su historia se desarrolla en la Tercera Edad del Sol de la Tierra Media, un lugar ficticio poblado por hombres y otras razas antropomorfas como los hobbits, los elfos o los enanos, así como por muchas otras criaturas reales y fantásticas. La novela narra el viaje del protagonista principal, Frodo Bolsón, hobbit de la Comarca, para destruir el Anillo Único y la consiguiente guerra que provocará el enemigo para recuperarlo, ya que es la principal fuente de poder de su creador, el Señor oscuro Sauron.",
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 4,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black.withOpacity(0.70),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 40,
                   ),
                 ],
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 60,
-          ),
-          Container(
-            width: pyth * 0.25,
-            height: pyth * 0.18,
-            decoration: BoxDecoration(
-              color: const Color(0xffB2DFDC),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Stack(
-              clipBehavior: Clip.none,
-              alignment: Alignment.center,
-              children: [
-                Positioned(
-                  bottom: pyth * 0.028,
-                  child: Container(
-                    height: pyth * 0.19,
-                    width: pyth * 0.14,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.35),
-                          blurRadius: 12,
-                          offset: const Offset(4, 4),
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.amber,
-                      image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          "https://images.pexels.com/photos/2954199/pexels-photo-2954199.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/item_slider_widgets.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ItemSliderWidget extends StatelessWidget {
+  Map<int, Color> colors = {
+    1: const Color(0xffFFC2D0),
+    2: const Color(0xffFFCE7F),
+    3: const Color(0xffffc8dd),
+    4: const Color(0xffa3b18a),
+    5: const Color(0xffb5838d),
+    6: const Color(0xffa7c957),
+    7: const Color(0xffa5a58d),
+    8: const Color(0xffb08968),
+    9: const Color(0xfff1c0e8),
+    10: const Color(0xffa78a7f),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    double pyth = sqrt(pow(height, 2) + pow(width, 2));
+
+    int index = (Random().nextInt(10) + 1);
+
+    return Container(
+      margin: EdgeInsets.only(right: 16, top: pyth * 0.038),
+      width: pyth * 0.25,
+      child: Column(
+        children: [
+          Container(
+            width: pyth * 0.25,
+            height: pyth * 0.18,
+            decoration: BoxDecoration(
+              color: colors[index],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  bottom: pyth * 0.028,
+                  child: Container(
+                    height: pyth * 0.19,
+                    width: pyth * 0.14,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.35),
+                          blurRadius: 12,
+                          offset: const Offset(4, 4),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.amber,
+                      image: const DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          "https://images.pexels.com/photos/2954199/pexels-photo-2954199.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                const Text(
+                  "The Lord if the  ring: Chacter 2. Lorem 12314 hgutjahdvhjd",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    height: 1.1,
+                  ),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  "Sonia Justiniano",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black.withOpacity(0.65),
+                    height: 1.1,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
